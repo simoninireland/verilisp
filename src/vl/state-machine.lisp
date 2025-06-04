9863998829a72644c6e81d1fdee1f1a691d898da
Simon Dobson <simonininreland@gmail.com>
2025-06-04 08:35:58 +0100

    First ideas

--- src/vl/state-machine.lisp
+++ src/vl/state-machine.lisp
@@ -21,163 +21,33 @@
 (declaim (optimize debug))
 
 
-;; ---------- Special states ------------------
+(defun typecheck-sm-state (state)
+  "Typecheck a single machine STATE description."
+  (destructuring-bind (name &rest body)
+      state
 
-(defvar *special-states* '(:before :after)
-  "The \"special\" states of a state machine, used
-for its internal working.")
+    ;; states must be integers -- should generalise to symbols too
+    (ensure-subtype name 'unsigned-byte)
 
-
-;; ---------- Parser helpers ----------
-
-(defun extract-state-labels (body)
-  "Return a list of state labels from BODY.
-
-Each clause in BODY is a list consisting of a label and one or more
-forms making up the code executed in that state."
-  (mapcar #'car body))
+    ;; the type of the state is the type of the last form
+    (typecheck `(progn ,@body))))
 
 
-(defun special-state-p (state)
-  "Test whether STATE is \"special\".
-
-Special states are used internally in building the behaviour of the
-state machine."
-  (member state *special-states*))
+(defmethod typecheck-sexp ((fun (eql 'state-machine) args))
+  (let ((tys (mapcar #'typecheck-sm-state args)))
+    (last tys)))
 
 
-(defun extract-user-state-labels (body)
-  "Return a list of state labels not including the special states."
-  (remove-if #'special-state-p
-	     (extract-state-labels body)))
+(defmethod synthesise-sexp ((fun (eql 'state-machine)) args)
+  (with-gensyms (state)
+    (let ((sm `(@ (posedge clk)
+		  (case ,state
+		    ,@args)
+		  )))
+
+      (synthesise sm))))
 
 
-(defun next-state-p (state labels)
-  "Test whether STATE is a valid next state in LABELS.
+(defmethod synthesise-sexp ((fun (eql 'next-state)) args)
 
-:BEFORE and :AFTER are never valid next states."
-  (and (member state labels)
-       (not (special-state-p state))))
-
-
-(defun ensure-next-state (state labels)
-  "Ensure that STATE is a valid next state in LABELS.
-
-Signal a STATE-MACHINE-MISMATCH error if STATE is invalid as a next
-state, either because it is unknown or because it is special.."
-  (unless (next-state-p state labels)
-    (error 'state-machine-mismatch :state state
-				   :hint "Make sure the target state is defined in the state machine")))
-
-
-(defun action-for-state (state body)
-  "Return the forms that constitute the actions of STATE in BODY.
-
-This can be used to access the actions of both user and special
-states."
-  (if-let ((m (assoc state body)))
-    (cdr m)))
-
-
-;; ---------- Top-level macros ----------
-
-;; We make use of the DSL and intercept the macro-expander directly
-;; so we can contextually expand the macros within.
-;;
-;; It's still a macro, though, because it expands the Verilisp source
-;; into more Verilisp source, and doesn't complicate the core. It does
-;; however need more macro-expansion machinery than normal.
-
-(defvar *current-machine-state* nil
-  "Variable holding the current state of the current state machine.")
-
-
-(defvar *current-machine-initial-state* nil
-  "Variable holding the initial state of the current state machine.")
-
-
-(defvar *current-machine-states* nil
-  "Variable holding all the states of the current state machine.")
-
-
-(defvar *surrounding-machine-state* nil
-  "Variable holding the current state of the enclosing state machine.")
-
-
-(defvar *surrounding-machine-states* nil
-  "Variable holding all the state of the enclosing state machine.")
-
-
-(defmethod expand-macros-sexp ((fun (eql 'state-machine)) args)
-  "Construct a state machine.
-
-BODY defines a set of state/action pairs roughly in the style of CASE.
-Each entry is a list with a symbol (the state label) as its head and
-the rest being the action code executed when the machine executes that
-state.
-
-The action in a state may include occurrences of the NEXT macro
-which change the machine's next state. Otherwise the next time
-the machine turns it will remain in the same state.
-
-There are two labels reserved for special purposes. The actions in
-the :BEFORE state happen before each turn of the state machine, and
-so can affect the state that will execute. The :AFTER state happens
-every time the machine turns."
-  (declare (optimize debug))
-  (destructuring-bind (&rest body)
-      args
-    (let* ((user-state-labels (extract-user-state-labels body))
-	   (initial-state (car user-state-labels))
-	   (user-state-decls (mapcar (lambda (state i)
-				       (list state i :as :constant))
-				     user-state-labels
-				     (iota (length user-state-labels))))
-	   (before-forms (action-for-state :before body))
-	   (after-forms (action-for-state :after body))
-	   (state-actions (remove-if (lambda (clause)
-				       (special-state-p (car clause)))
-				     body)))
-
-      (with-gensyms (state)
-	(let* ((*surrounding-machine-state* *current-machine-state*)
-	       (*surrounding-machine-states* *current-machine-states*)
-	       (*current-machine-state* state)
-	       (*current-machine-initial-state* initial-state)
-	       (*current-machine-states* user-state-labels))
-
-	  ;; recursively expand the body forms
-	  (expand-macros `(let ,user-state-decls
-			    (let ((,state ,initial-state))
-
-			      ;; actions always performed before each turn (if any)
-			      ,@before-forms
-
-			      ;; the state machine proper
-			      (case ,state
-				,@state-actions)
-
-			      ;; actions always performed after each turn (if any)
-			      ,@after-forms))))))))
-
-
-(defmacro next/vl (next-state)
-  "Move the current machine to NEXT-STATE."
-  (ensure-next-state next-state *current-machine-states*)
-  `(setq ,*current-machine-state* ,next-state))
-
-
-(defmacro exit/vl (&optional exit-state)
-  "Exit the current machine to EXIT-STATE of the surrounding machine.
-
-The current machine is returned to its initial state.
-
-If EXIT-STATE is omitted, the state of the surrounding machine is left
-unchanged. The net result will be that, the next time the surrounding
-machine turns, it will re-enter the current machine in that machine's
-initial state."
-  `(progn
-     (setq ,*current-machine-state* ,*current-machine-initial-state*)
-     ,(when exit-state
-	  (ensure-next-state exit-state *surrounding-machine-states*)
-	  `(setq ,*surrounding-machine-state* ,exit-state))))
+  )
--- test/test-vl-state-machine.lisp
+++ test/test-vl-state-machine.lisp
@@ -1,4 +1,4 @@
-;; Tests of STATE-MACHINE macros
+;; Tests of state machine construction
 ;;
 ;; Copyright (C) 2024--2025 Simon Dobson
 ;;
@@ -21,132 +21,24 @@
 (in-suite verilisp/vl)
 
 
-;; It's tricky to test the output automatically, so we
-;; only test for errors.
-
-(test test-state-machine-jumps
-  "Test we can jump within a state machine."
-  (is (vl:expand-macros '(vl:state-machine
-			   (test-even
-			    (incf a)
-			    (vl::next test-odd))
-			   (test-odd
-			    (incf a)
-			    (if (> a 10)
-				(vl::next test-idle)
-				(vl::next test-even)))
-			   (test-idle)))))
-
-
-(test test-state-machine-nested
-  "Test we can expand a nested pair of a state machines."
-  (is (vl:expand-macros '(vl:state-machine
-			(test-even
-			 (incf a)
-			 (vl::next test-odd))
-			(test-odd
-			 (incf a)
-			 (if (> a 10)
-			     (vl::next test-idle)
-
-			     (vl:state-machine
-			       (nested-one
-				(vl::next nested-two))
-			       (nested-two
-				(setq a 5)
-				(vl::exit test-even)))))))))
-
-
-(test test-state-machine-reenter
-  "Test we can re-enter a nested machine."
-  (is (vl:expand-macros '(vl:state-machine
-			    (test-even
-			     (incf a)
-			     (vl::next test-odd))
-			    (test-odd
-			     (incf a)
-			     (if (> a 10)
-				 (vl::next test-idle)
-
-				 (vl:state-machine
-				   (nested-one
-				    (vl::next nested-two))
-				   (nested-two
-				    (setq a 5)
-				    (vl::exit)))))))))
-
-
-(test test-state-machine-before-after
-  "Test we include state machine before and after code."
-  (is (vl:expand-macros '(vl:state-machine
-			   (:before
-			    (setf a 0))
-			   (:after
-			    (setf a 255))
-			   (test-even
-			    (incf a)
-			    (vl::next test-odd))
-			   (test-odd
-			    (incf a)
-			    (if (> a 10)
-				(vl::next test-odd)
-				(vl::next test-even)))))))
-
-
-(test test-state-machine-not-state
-  "Test we detect jumping to an undefined state."
-  (signals (vl:state-machine-mismatch)
-    (vl:expand-macros '(vl:state-machine
-			 (test-even
+(test test-sm-linear
+  "Test we can create a simple linear machine."
+  (let ((p (copy-tree '(let ((a 1))
+			(vl:state-machine (posedge clk)
+			 (0
 			  (incf a)
-			  (vl::next test-ttt))
-			 (test-odd
-			  (incf a))))))
+			  (next-state 1))
+			 (1
+			  (if (> a 5)
+			      (next-state 2)
+			      (next-state 1)))
+			 (2
+			  t))))))
+
+    (vl:typecheck p)
+
+    )
 
 
-(test test-state-machine-not-before
-  "Test we detect jumping to a special state."
-  (signals (vl:state-machine-mismatch)
-    (vl:expand-macros '(vl:state-machine
-			 (test-even
-			  (incf a)
-			  (vl::next test-ttt))
-			 (:before
-			  (setf a 0))
-			 (test-odd
-			  (incf a)
-			  (vl::next :before))))))
 
-
-(test test-state-machine-not-surrounding-state
-  "Test we detect exiting to an undefined state in the surrounding machine."
-  (signals (vl:state-machine-mismatch)
-    (vl:expand-macros '(vl:state-machine
-			 (test-even
-			  (incf a)
-			  (vl::next test-odd))
-			 (test-odd
-			  (incf a)
-			  (vl:state-machine
-			      (nested-one
-			       (vl::next nested-two))
-			      (nested-two
-			       (setq a 5)
-			       (vl::exit test-ttt))))))))
-
-
-(test test-state-machine-not-next-state
-  "Test we detect trying to jump (rather than exit) to a state in the surrounding machine."
-  (signals (vl:state-machine-mismatch)
-    (vl:expand-macros '(vl:state-machine
-			 (test-even
-			  (incf a)
-			  (vl::next test-odd))
-			 (test-odd
-			  (incf a)
-			  (vl:state-machine
-			    (nested-one
-			     (vl::next nested-two))
-			    (nested-two
-			     (setq a 5)
-			     (vl::next test-even))))))))
+  )
