d7c9b8b50e1fc14982e5d5d7638b70028acc1a99
Simon Dobson <simonininreland@gmail.com>
2025-08-29 15:54:41 +0100

    Simplified and made even more Lisp-like

--- examples/risc-v/basic/core.lisp
+++ examples/risc-v/basic/core.lisp
@@ -18,7 +18,7 @@
 ;; along with verilisp. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 (defmodule/vl SOC (clk-in
-		   ;; (reset-in :as :wire :type (unsigned-byte 1) :direction :in)
+		   ;; reset-in
 		   leds-out
 		   rxd txd)
   (declare (type bit clk-in rxd txd)
@@ -26,20 +26,20 @@
 
   (let (clk reset
 
-	;; plug in to the output to visualise
-	leds
+	    ;; plug in to the output to visualise
+	    leds
 
-	;; core state
-	(mem  (make-array '(256) :element-type (unsigned-byte 32)
-				 :initial-contents (:file "firmware.hex")))
-	pc instr
+	    ;; core state
+	    (mem  (make-array '(256) :element-type (unsigned-byte 32)
+				     :initial-contents (:file "firmware.hex")))
+	    pc instr
 
-	;; clock management
-	(cw (make-instance 'clockworks :clk-in clk-in
-				       :reset-in 0
-				       :clk clk
-				       :reset reset
-				       :slow 19)))
+	    ;; clock management
+	    (cw (make-instance 'clockworks :clk-in clk-in
+					   :reset-in 0
+					   :clk clk
+					   :reset reset
+					   :slow 19)))
     (declare (type bit clk reset)
 	     (type (unsigned-byte 5) leds)
 	     (type (unsigned-byte 32) pc instr))
@@ -48,132 +48,128 @@
     (setq leds-out leds)
 
     ;; instruction decoding
-    (let ((isALUreg (= (bref instr 6 :end 0) #2r0110011))
-	  (isALUimm (= (bref instr 6 :end 0) #2r0010011))
-	  (isBranch (= (bref instr 6 :end 0) #2r1100011))
-	  (isJALR   (= (bref instr 6 :end 0) #2r1100111))
-	  (isJAL    (= (bref instr 6 :end 0) #2r1101111))
-	  (isAIUPC  (= (bref instr 6 :end 0) #2r0010111))
-	  (isLUT    (= (bref instr 6 :end 0) #2r0110111))
-	  (isLoad   (= (bref instr 6 :end 0) #2r0000011))
-	  (isStore  (= (bref instr 6 :end 0) #2r0100011))
-	  (isSystem (= (bref instr 6 :end 0) #2r1110011))
+    (with-bitfields ((funct7 7) (rs2Id 5) (rs1Id 5) (funct3 3) (rdId 5) (opcode 7))
+      instr
 
-	  ;; immediate formats
-	  (Uimm (make-bitfields (bref instr 31)
-				(bref instr 30 :end 12)
-				(extend-bits 0 12)))
-	  (Iimm (make-bitfields (extend-bits (bref instr 31) 21)
-				(bref instr 30 :end 20)))
-	  (Simm (make-bitfields (extend-bits (bref instr 31) 21)
-				(bref instr 30 :end 25)
-				(bref instr 11 :end 7)))
-	  (Bimm (make-bitfields (extend-bits (bref instr 31) 20)
-				(bref instr 7)
-				(bref instr 30 :end 25)
-				(bref instr 11 :end 8)
-				(extend-bits 0 1)))
-	  (Jimm (make-bitfields (extend-bits (bref instr 31) 12)
-				(bref instr 19 :end 12)
-				(bref instr 20)
-				(bref instr 30 :end 21)
-				(extend-bits 0 1)))
+      (let (;; instruction classes
+	    (ALUreg-p (= opcode #2r0110011))
+	    (ALUimm-p (= opcode #2r0010011))
+	    (branch-p (= opcode #2r1100011))
+	    (JALR-p   (= opcode #2r1100111))
+	    (JAL-p    (= opcode #2r1101111))
+	    (AIUPC-p  (= opcode #2r0010111))
+	    (LUT-p    (= opcode #2r0110111))
+	    (load-p   (= opcode #2r0000011))
+	    (store-p  (= opcode #2r0100011))
+	    (system-p (= opcode #2r1110011))
 
-	  ;; source and destination registers
-	  (rs1Id (bref instr 19 :end 15))
-	  (rs2Id (bref instr 24 :end 20))
-	  (rdId  (bref instr 11 :end 7))
+	    ;; immediate formats
+	    (Uimm (make-bitfields (bref instr 31)
+				  (bref instr 30 :end 12)
+				  (extend-bits 0 12)))
+	    (Iimm (make-bitfields (extend-bits (bref instr 31) 21)
+				  (bref instr 30 :end 20)))
+	    (Simm (make-bitfields (extend-bits (bref instr 31) 21)
+				  (bref instr 30 :end 25)
+				  (bref instr 11 :end 7)))
+	    (Bimm (make-bitfields (extend-bits (bref instr 31) 20)
+				  (bref instr 7)
+				  (bref instr 30 :end 25)
+				  (bref instr 11 :end 8)
+				  (extend-bits 0 1)))
+	    (Jimm (make-bitfields (extend-bits (bref instr 31) 12)
+				  (bref instr 19 :end 12)
+				  (bref instr 20)
+				  (bref instr 30 :end 21)
+				  (extend-bits 0 1))))
 
-	  ;; function codes
-	  (funct3 (bref instr 14 :end 12))
-	  (funct7 (bref instr 31 :end 25)))
+	;; register file and working registers
+	(let ((register-file (make-array '(32) :element-type (unsigned-byte 32)
+					       :initial-element 0))
+	      (rs1           0)
+	      (rs2           0))
 
-      ;; register bank
-      (let ((RegisterBank  (make-array '(32) :element-type (unsigned-byte 32)))
-	    (rs1           0)
-	    (rs2           0))
+	  ;; the ALU
+	  (let ((aluIn1 rs1)
+		(aluIn2 (if ALUreg-p
+			    rs2
+			    Iimm))
+		(shamt (if ALUreg-p
+			   (bref rs2 4 :end 0)
+			   (bref instr 24 :end 20)))
+		aluOut)
 
-	;; the ALU
-	(let((aluIn1 rs1)
-	     (aluIn2 (if isALUreg
-			 rs2
-			 Iimm))
-	     (shamt (if isALUreg
-			(bref rs2 4 :end 0)
-			(bref instr 24 :end 20)))
-	     aluOut)
+	    (@ (*)
+	       (case funct3
+		 (#2r000
+		  (if (logand (bref funct7 5)
+			      (bref instr 5))
+		      (setq aluOut (- aluIn1 aluIn2))
+		      (setq aluOut (+ aluIn1 aluIn2))))
 
-	  (@ (*)
-	     (case funct3
-	       (#2r000
-		(if (logand (bref funct7 5)
-			    (bref instr 5))
-		    (setq aluOut (- aluIn1 aluIn2))
-		    (setq aluOut (+ aluIn1 aluIn2))))
+		 (#2r001
+		  (setq aluOut (<< aluIn1 shamt)))
 
-	       (#2r001
-		(setq aluOut (<< aluIn1 shamt)))
+		 (#2r010
+		  (setq aluOut (< (the 'signed-byte aluIn1)
+				  (the 'signed-byte aluIn2)))) ;; signed
 
-	       (#2r010
-		(setq aluOut (< (the 'signed-byte aluIn1)
-				(the 'signed-byte aluIn2)))) ;; signed
+		 (#2r011
+		  (setq aluOut (< aluIn1 aluIn2))) ;; unsigned
 
-	       (#2r011
-		(setq aluOut (< aluIn1 aluIn2))) ;; unsigned
+		 (#2r100
+		  (setq aluOut (logxor aluIn1 aluIn2)))
 
-	       (#2r100
-		(setq aluOut (logxor aluIn1 aluIn2)))
+		 (#2r101
+		  (if (bref funct7 5)
+		      (setq aluOut (>> (the 'signed-byte aluIn1) shamt)) ;; sign-extended
+		      (setq aluOut (>> aluIn1 shamt)))) ;; unsigned
 
-	       (#2r101
-		(if (bref funct7 5)
-		    (setq aluOut (>> (the 'signed-byte aluIn1) shamt)) ;; sign-extended
-		    (setq aluOut (>> aluIn1 shamt)))) ;; unsigned
+		 (#2r110
+		  (setq aluOut (logior aluIn1 aluIn2)))
 
-	       (#2r110
-		(setq aluOut (logior aluIn1 aluIn2)))
+		 (#2r111
+		  (setq aluOut (logand aluIn1 aluIn2)))))
 
-	       (#2r111
-		(setq aluOut (logand aluIn1 aluIn2)))))
+	    ;; the state machine
+	    (@ (posedge clk)
+	       (tagbody
+		fetch-instruction
+		  ;; State 0: fetch the next instruction
+		  (if reset
+		      (setq pc 0))
 
-	  ;; the state machine
-	  (@ (posedge clk)
-	     (tagbody
-	      fetch-instruction
-		;; State 0: fetch the next instruction
-		(if reset
-		    (setq pc 0))
+		  (setq instr (aref mem (bref pc 31 :end 2)))
 
-		(setq instr (aref mem (bref pc 31 :end 2)))
+		fetch-registers
+		  ;; State 1: load from registers
+		  (setq rs1 (aref register-file rs1Id))
+		  (setq rs2 (aref register-file rs2Id))
 
-	      fetch-registers
-		;; State 1: load from registers
-		(setq rs1 (aref RegisterBank rs1Id))
-		(setq rs2 (aref RegisterBank rs2Id))
+		execute-writeback
+		  ;; State 2: execute the instruction and write back results
 
-	      execute-writeback
-		;; State 2: execute the instruction and write back results
+		  ;; writeback data to registers
+		  (when (and (or ALUReg-p
+				 ALUImm-p
+				 JAL-p
+				 JALR-p)
+			     (/= rdId 0)) ; don't update R0, which is constantly 0
+		    (setf (aref register-file rdId) (if (or JAL-p JALR-p)
+							(+ pc 4)
+							aluOut))
 
-		;; writeback data to registers
-		(when (and (or isALUReg
-			       isALUImm
-			       isJAL
-			       isJALR)
-			   (/= rdId 0))  ; don't update R0, which is constantly 0
-		  (setf (aref RegisterBank rdId) (if (or isJAL isJALR)
-						     (+ pc 4)
-						     aluOut))
+		    ;; update the LEDS if writing to R1
+		    (when (= rdId 1)
+		      (setq leds aluOut)))
 
-		  ;; update the LEDS if writing to R1
-		  (when (= rdId 1)
-		    (setq leds aluOut)))
+		  ;; update PC
+		  (when (not system-p)
+		    (setq pc (cond (JAL-p
+				    (+ pc Jimm))
+				   (JALR-p
+				    (+ rs1 Iimm))
+				   (t
+				    (+ pc 4)))))
 
-		;; update PC
-		(when (not isSystem)
-		  (setq pc (cond (isJAL
-				  (+ pc Jimm))
-				 (isJALR
-				  (+ rs1 Iimm))
-				 (t
-				  (+ pc 4)))))
-
-		(go fetch-instruction))))))))
+		  (go fetch-instruction)))))))))
