707cf2f18bc5435fc7f27eadffa4299d6e1f977e
Simon Dobson <simonininreland@gmail.com>
2025-09-11 13:30:17 +0100

    Changed output of types for mismatches

--- src/vl/conditions.lisp
+++ src/vl/conditions.lisp
@@ -362,8 +362,8 @@ almost certainly a logical error."))
     :reader received-type))
   (:report (lambda (c str)
 	     (format-condition-context (format nil "Expected a value of type ~a, got one of type ~a"
-					       (lub (expected-type c))
-					       (lub (received-type c)))
+					       (eval-type (expected-type c))
+					       (eval-type (received-type c)))
 				       c str)))
   (:documentation "Condition signalled when types don't match.
 
