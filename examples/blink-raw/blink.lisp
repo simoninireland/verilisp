a8d9f7318b45a261d6da74f9922b3e2a9258bdd5
Simon Dobson <simonininreland@gmail.com>
2026-05-15 16:24:41 +0100

    Updated to flash on-stick LEDs

--- examples/blink-raw/blink.lisp
+++ examples/blink-raw/blink.lisp
@@ -1,24 +1,24 @@
-;; A simple LED blinker
-;;
-;; Copyright (C) 2024--2025 Simon Dobson
-;;
-;; This file is part of cl-vhdsl, a Common Lisp DSL for hardware design
-;;
-;; cl-vhdsl is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; cl-vhdsl is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with cl-vhdsl. If not, see <http://www.gnu.org/licenses/gpl.html>.
+;;;; A simple LED blinker
+;;;;
+;;;; Copyright (C) 2024--2026 Simon Dobson
+;;;;
+;;;; This file is part of verilisp, a very Lisp approach to hardware synthesis
+;;;;
+;;;; verilisp is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; verilisp is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with verilisp. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 (defmodule/vl blink (clk leds
-		     &key (bits 2) (delay 22))
+		     &key (bits 5) (delay 24))
   (declare (type bit clk)
 	   (width bits leds))
 
--- examples/blink-sm/blink.lisp
+++ examples/blink-sm/blink.lisp
@@ -1,43 +1,42 @@
-;; An LED-blinking state machine
-;;
-;; Copyright (C) 2024--2025 Simon Dobson
-;;
-;; This file is part of cl-vhdsl, a Common Lisp DSL for hardware design
-;;
-;; cl-vhdsl is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; cl-vhdsl is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with cl-vhdsl. If not, see <http://www.gnu.org/licenses/gpl.html>.
+;;;; An LED-blinking state machine
+;;;;
+;;;; Copyright (C) 2024--2027 Simon Dobson
+;;;;
+;;;; This file is part of verilisp, a very Lisp approach to hardware synthesis
+;;;;
+;;;; verilisp is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; verilisp is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with verilisp. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 (defmodule/vl blink (clk leds
 		     &key (bits 5) (delay 22))
   (declare (type bit clk)
 	   (width bits leds))
 
-  (let ((out 0))
-    (declare (width bits out))
+  (let ((out 0)
+	(counter 1))
+    (declare (width bits out)
+	     (type (unsigned-byte delay) counter))
 
     (@ (posedge clk)
-       (let ((counter 1))
-	 (declare (type (unsigned-byte delay) counter))
+       (forever
+	;; increment the counter until we roll-over
+	(while (> counter 0)
+	       (incf counter))
 
-	 (forever
-	  ;; increment the counter until we roll-over
-	  (while (> counter 0)
-		 (incf counter))
+	;; change the LEDs
+	(setq out (1+ out))
 
-	  ;; change the LEDs
-	  (setq out (1+ out))
-
-	  ;; reset the counter
-	  (setq counter 1))))
+	;; reset the counter
+	(setq counter 1)))
 
     (setf leds out)))
